8c06ada90921fca3930bffbdd8779b06b1144772
Maximilian-Pichler <max_pichler@hotmail.de>
2021-02-25 13:00:38 +0100

    updated multi

--- 2021/practice_round/test run.py
+++ 2021/practice_round/test run.py
@@ -3,13 +3,13 @@ import time
 
 #%%
 start1 = time.time()
-!python3 practice_m.py
+!python3 practice.py
 end1 = time.time()
 # %%
 start2 = time.time()
-!python3 practice.py
+!python3 practice_m.py
 end2 = time.time()
 # %%
-print(f'multi-core execution time {end1 - start1}')
-print(f'single-core execution time {end2 - start2}')
+print(f'single-core execution time {end1 - start1}')
+print(f'multi-core execution time {end2 - start2}')
 # %%
